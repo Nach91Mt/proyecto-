161c1836ea985a0569b394c90b64266c13e30b08
Lopez Mollinedo <ilmollinedo@indra.es>
2023-05-04 16:35:11 +0200

    version 0.1.2

--- api.py
+++ api.py
@@ -11,24 +11,26 @@ datos['leds']=0
 @app.route('/')
 
 def api():
+    i=1
+    if i < 2:
+        print('yes')
         #ejemplo http://127.0.0.1:5000/?id=Nacho&ilu=22
         #recogemos los datos introducidos por la app del movil
     ilu=request.args.get('ilu')  
     pro=request.args.get('pro')
     id= request.args.get('id')
     led=request.args.get('leds')
-
     #guardamos los datos introducidos por la app
-    if ilu != None:
+    if ilu != None :
         datos['ilu']=ilu
-    if pro != None:
+    if pro != None :
         datos['pro']=pro
-    if led != None:
+    if led != None :
         datos['leds']=led
     
     if id == "Nacho":
         return render_template('index.html',datos=datos)
     else:
-        return "Usuario incorrecto"
+        return "<h1>Usuario incorrecto</h1>"
 if __name__ =='__main__':
     app.run(debug=True)
