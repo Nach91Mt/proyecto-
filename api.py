0ad4d9b5f6809bafecbeaf3acbfe2d84b85a4e97
Lopez Mollinedo <ilmollinedo@indra.es>
2023-05-17 13:13:26 +0200

    v2.2

--- api.py
+++ api.py
@@ -37,8 +37,6 @@ def api():
         return render_template('index.html',datos=datos)
     else:
         return "<h1 >Usuario incorrecto</h1>"
-@app.route('/datos')
-def obtener_datos():
-    return jsonify(datos)
+
 if __name__ =='__main__':
     app.run(debug=True)
